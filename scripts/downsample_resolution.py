8dd1c912c41de323b84c3168388c7234522d6d9e
pianwan <pianwan@outlook.com>
2023-08-24 18:30:34 +0800

    Fix file name

--- scripts/downsample_resolution.py
+++ scripts/downsample_resolution.py
@@ -4,22 +4,25 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    imgdir = os.path.expanduser("D:\\EXP_ORIGINAL\\LivingRoom\\camera\\temp")
-    outdir = os.path.expanduser("D:\\EXP_ORIGINAL\\LivingRoom\\camera\\half_reso")
+    imgdir = os.path.expanduser(r"D:\wp-gen\livingroom\camera\temp")
+    outdir = os.path.expanduser(r"D:\wp-gen\livingroom\camera\temp_half_resolution")
+    os.makedirs(outdir, exist_ok=True)
     rate = 2
 
     imgfiles = [os.path.join(imgdir, f) for f in sorted(os.listdir(imgdir))]
     imgfiles = [f for f in imgfiles if any([f.endswith(ex) for ex in ['JPG', 'jpg', 'png', 'jpeg', 'PNG']])]
 
-    imgs = [cv2.imread(f) for f in imgfiles]
+    img = cv2.imread(imgfiles[0])
 
-    hw = imgs[0].shape[:2]
+    hw = img.shape[:2]
 
     h2 = hw[0] // rate
     w2 = hw[1] // rate
 
-    for i, img in enumerate(imgs):
+    for i, f in enumerate(imgfiles):
+        img = cv2.imread(f)
+        name = os.path.splitext(os.path.basename(f))[0]
         img_out = cv2.resize(img, (w2, h2), interpolation=cv2.INTER_LINEAR)
-        cv2.imwrite(os.path.join(outdir, f"{i:06d}.png"), img_out)
+        cv2.imwrite(os.path.join(outdir, f"{name}.png"), img_out)
 
     print("Finished")
