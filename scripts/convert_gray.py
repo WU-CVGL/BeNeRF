90ec42cc13e5f3cf0987eb8c0901992e880642ab
pianwan <pianwan@outlook.com>
2023-08-25 14:37:06 +0800

    Makedirs

--- scripts/convert_gray.py
+++ scripts/convert_gray.py
@@ -3,11 +3,13 @@ import os.path
 import cv2
 
 if __name__ == '__main__':
-    indir = os.path.expanduser("D:\\EXP_ORIGINAL\\REAL\\blur_demo\\images_undistorted_blur")
-    outdir = os.path.expanduser("D:\\EXP_ORIGINAL\\REAL\\blur_demo\\images_undistorted_blur_gray")
+    indir = os.path.expanduser(r"D:\blender\scripts\BlenderCV\gttanabata_test1\raw")
+    outdir = os.path.expanduser(r"D:\blender\scripts\BlenderCV\gttanabata_test1\raw_gray")
 
     imgfiles = [os.path.join(indir, f) for f in sorted(os.listdir(indir))]
     imgfiles = [f for f in imgfiles if any([f.endswith(ex) for ex in ['JPG', 'jpg', 'png', 'jpeg', 'PNG']])]
+
+    os.makedirs(outdir, exist_ok=True)
 
     for i, imgfile in enumerate(imgfiles):
         gray = cv2.imread(imgfile, cv2.IMREAD_GRAYSCALE)
