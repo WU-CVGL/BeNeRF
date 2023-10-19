65a215122dc4ad247e9cca975d41489513b01104
pianwan <pianwan@outlook.com>
2023-10-19 21:38:37 +0800

    Fix naming

--- scripts/convert_gray.py
+++ scripts/convert_gray.py
@@ -3,8 +3,8 @@ import os.path
 import cv2
 
 if __name__ == '__main__':
-    indir = os.path.expanduser(r"D:\blender\scripts\BlenderCV\gttanabata_test1\raw")
-    outdir = os.path.expanduser(r"D:\blender\scripts\BlenderCV\gttanabata_test1\raw_gray")
+    indir = os.path.expanduser(r"I:\wp-event\real\board\images")
+    outdir = os.path.expanduser(r"I:\wp-event\real\board\images_gray")
 
     imgfiles = [os.path.join(indir, f) for f in sorted(os.listdir(indir))]
     imgfiles = [f for f in imgfiles if any([f.endswith(ex) for ex in ['JPG', 'jpg', 'png', 'jpeg', 'PNG']])]
@@ -12,5 +12,6 @@ if __name__ == '__main__':
     os.makedirs(outdir, exist_ok=True)
 
     for i, imgfile in enumerate(imgfiles):
+        name = os.path.splitext(os.path.basename(imgfile))[0]
         gray = cv2.imread(imgfile, cv2.IMREAD_GRAYSCALE)
-        cv2.imwrite(os.path.join(outdir, f"{i:6d}.png"), gray)
+        cv2.imwrite(os.path.join(outdir, f"{name}.png"), gray)
