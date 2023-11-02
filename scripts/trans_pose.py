81fb86e6f2d3f5c305c820ec421dc49e745f4712
pianwan <pianwan@outlook.com>
2023-11-03 02:52:56 +0800

    Add whiteroom, pinkcastle, tanabata_1

--- scripts/trans_pose.py
+++ scripts/trans_pose.py
@@ -9,10 +9,24 @@ import spline
 transdir = os.path.expanduser(r"../logs/trans.npy")
 
 if __name__ == '__main__':
-    # Origin c1->w
-    R = np.array([0.976781, 0.013642, 0.203538, -0.065466])
-    t = np.array([-153.531555, -33.654785, 151.457611]) * 0.01
+    # Living room Origin c1->w
+    # R = np.array([0.976781, 0.013642, 0.203538, -0.065466])
+    # t = np.array([-153.531555, -33.654785, 151.457611]) * 0.01
+
+    # White room Origin c1->w
+    # R = np.array([0.957979, 0.040439, 0.226936, -0.170707])
+    # t = np.array([-54.199661, -22.093983, 150.290070]) * 0.01
+    # origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
+
+    # pinkcastle  room Origin c1->w
+    R = np.array([0.999356, 0.000100, -0.035774, 0.002800])
+    t = np.array([4900.291504, 0.000000, 1622.000732]) * 0.01
     origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
+
+    # tanabata  room Origin c1->w
+    # R = np.array([0.6097474694252014, 0.42423614859580994, 0.382367879152298, 0.5495694279670715])
+    # t = np.array([15.299233436584473, -3.744455337524414, 11.704211235046387])
+    # origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
 
     # Trans c2->c1
     R_t = np.array([[1., 0., 0.],
