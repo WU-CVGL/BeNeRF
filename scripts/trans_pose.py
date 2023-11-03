e9c34109c333bb50f8e196195897c1a949da7b58
pianwan <pianwan@outlook.com>
2023-11-03 18:09:04 +0800

    Add outdoorpool

--- scripts/trans_pose.py
+++ scripts/trans_pose.py
@@ -18,15 +18,20 @@ if __name__ == '__main__':
     # t = np.array([-54.199661, -22.093983, 150.290070]) * 0.01
     # origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
 
-    # pinkcastle  room Origin c1->w
-    R = np.array([0.999356, 0.000100, -0.035774, 0.002800])
-    t = np.array([4900.291504, 0.000000, 1622.000732]) * 0.01
-    origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
+    # pinkcastle   Origin c1->w
+    # R = np.array([0.999356, 0.000100, -0.035774, 0.002800])
+    # t = np.array([4900.291504, 0.000000, 1622.000732]) * 0.01
+    # origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
 
-    # tanabata  room Origin c1->w
+    # tanabata   Origin c1->w
     # R = np.array([0.6097474694252014, 0.42423614859580994, 0.382367879152298, 0.5495694279670715])
     # t = np.array([15.299233436584473, -3.744455337524414, 11.704211235046387])
     # origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
+
+    # ourdoorpool  Origin c1->w
+    R = np.array([0.449483186006546, 0.3870837092399597, 0.4603446424007416, 0.660464882850647])
+    t = np.array([16.65660285949707, 3.878607749938965, 3.990011215209961])
+    origin = pp.SE3(torch.tensor(np.concatenate((t, R[1:], [R[0]]))))
 
     # Trans c2->c1
     R_t = np.array([[1., 0., 0.],
