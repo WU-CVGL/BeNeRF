6f9ac8eccff51ef3d6ad8e3990f86b136c8d7ff7
pianwan <pianwan@outlook.com>
2023-11-09 22:44:21 +0100

    Add teaser

--- scripts/draw/crop_img.py
+++ scripts/draw/crop_img.py
@@ -64,13 +64,19 @@ if __name__ == '__main__':
     # x_end = [448, 677]
     # y_end = [403, 157]
 
-    # real-carpet-168
-    x_start = [851, 379, 179, 1156]
-    y_start = [459, 220, 851, 786]
-    x_end = [1123, 556, 299, 1377]
-    y_end = [625, 319, 982, 1037]
+    # tanabata: teaser (12)
+    x_start = [136]
+    y_start = [162]
+    x_end = [391]
+    y_end = [332]
 
-    path = r'C:\Users\User\Downloads\test'  # image folder path
+    # real-carpet-168
+    # x_start = [851, 379, 179, 1156]
+    # y_start = [459, 220, 851, 786]
+    # x_end = [1123, 556, 299, 1377]
+    # y_end = [625, 319, 982, 1037]
+
+    path = r'/Users/pianwan/Desktop/DataProcess/tanabata_teaser/img'  # image folder path
 
     files = find_files(path)
 
