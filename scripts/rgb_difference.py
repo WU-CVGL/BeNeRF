0bed6c80e1863b8761eb1626c600b7e130e57f83
pianwan <pianwan@outlook.com>
2023-11-11 02:31:11 +0800

    Fxi rgb diff

--- scripts/rgb_difference.py
+++ scripts/rgb_difference.py
@@ -4,19 +4,19 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    in_dir = os.path.expanduser(r"")
-    out_dir = os.path.expanduser(r"")
-    gt_file = os.path.expanduser(r"")
+    in_dir = os.path.expanduser(r"C:\Users\User\PycharmProjects\EventBADNeRF\logs\cup\35\images_test\img_test_080000")
+    out_dir = os.path.expanduser(r"C:\Users\User\Desktop\out\diff")
+    gt_file = os.path.expanduser(r"C:\Users\User\PycharmProjects\EventBADNeRF\data\cup\images\000035.png")
     files = [f for f in os.listdir(in_dir) if f.lower().endswith((".jpg", "png"))]
-    imgs = [cv2.imread(os.path.join(in_dir, f)) for f in files]
+    imgs = [cv2.imread(os.path.join(in_dir, f), cv2.IMREAD_GRAYSCALE) for f in files]
 
     syn = 0
     for i in imgs:
-        syn += i
+        syn += i.astype(int)
 
-    syn /= len(imgs)
-    cv2.imwrite(os.path.join(out_dir, "synthesized.png"), syn)
+    syn = syn / len(imgs)
+    cv2.imwrite(os.path.join(out_dir, "synthesized.png"), np.clip(syn, 0, 255).astype(np.uint8))
 
-    gt = cv2.imread(gt_file)
+    gt = cv2.imread(gt_file, cv2.IMREAD_GRAYSCALE)
     diff = np.absolute(syn - gt)
-    cv2.imwrite(os.path.join(out_dir, "diff.png"), diff)
+    cv2.imwrite(os.path.join(out_dir, "diff.png"), np.clip(diff * 10, 0, 255).astype(np.uint8))
