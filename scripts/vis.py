f2f453b1a4ade3f97b344db2ab051785ff3bcbfb
pianwan <pianwan@outlook.com>
2023-11-17 06:41:15 +0800

    vis

--- scripts/vis.py
+++ scripts/vis.py
@@ -10,9 +10,9 @@ def dvsarray_to_image(events, height, width, init_map=None):
     # Draw event image in RPG way
     if init_map is None:
         dvs_image = np.ones((width, height, 3), dtype=np.dtype("uint8"))
+        dvs_image *= 255
     else:
         dvs_image = init_map
-        dvs_image *= 255
     map = np.zeros((width, height))
     eventutils.accumulate_events_no_numba(map, events[:, 0].astype(int), events[:, 1].astype(int), events[:, 3])
     pos_map = map > 0
@@ -52,12 +52,12 @@ def vis_3d(events, height, width):
 
 
 if __name__ == '__main__':
-    events = np.load("/Users/pianwan/Downloads/blur_1019/carpet/2023-10-17-23-04-44/events/events.npy")
-    save_dir = "/Users/pianwan/Downloads/blur_1019/carpet/2023-10-17-23-04-44/output"  # image save path
-    ts = 1.697555087649756908e+09
-    te = 1.697555087726284504e+09
-    duration = (te - ts) / 18
-    te = ts + duration
+    events = np.load(r"C:\Users\User\PycharmProjects\EventBADNeRF\data\carpet\events\events.npy")
+    save_dir = r"C:\Users\User\PycharmProjects\EventBADNeRF\data\carpet\out"  # image save path
+    ts = 1.697555087686322927e+09 - 60e-3
+    te = ts + 20e-3
+    # duration = (te - ts) / 18
+    # te = ts + duration
 
     # ts = 1.697554819352672577e+09
     # te = 1.697554819433324575e+09
@@ -65,5 +65,9 @@ if __name__ == '__main__':
     # events =  # Nx4 ndarray
     i = 1
 
+    img = cv.imread(r"C:\Users\User\PycharmProjects\EventBADNeRF\data\carpet\images\000065.png")
+    img = cv2.resize(img, (640, 480), interpolation=cv2.INTER_LINEAR)
     e_img = dvsarray_to_image(events, 640, 480)
     cv.imwrite(f"{save_dir}/{i:0>10d}.png", cv.cvtColor(e_img, cv.COLOR_RGB2BGR))
+    img_all = cv2.addWeighted(e_img, 0.5, img, 0.9, 0.)
+    cv.imwrite(f"{save_dir}/{i:0>10d}.png", cv.cvtColor(img_all, cv.COLOR_RGB2BGR))
