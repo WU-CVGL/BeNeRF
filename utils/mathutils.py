6b38c16e247b7ee65370e43ef896c2585f8ee835
akawincent <liwenpu0520@gmail.com>
2023-12-15 21:40:08 +0800

    fix: - align dimension

--- utils/mathutils.py
+++ utils/mathutils.py
@@ -1,6 +1,9 @@
 import torch
 
 def safelog(x, eps=1e-12):
-    x = max(x, eps)
+    x[..., 0:1]
+    dim = x.size()
+    eps = torch.Tensor([eps]).expand(dim)
+    x = torch.max(x, torch.Tensor(eps))
     return torch.log(x)
 
