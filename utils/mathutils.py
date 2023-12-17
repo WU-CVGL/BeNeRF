edf4dc6d4871b5bce6b5a816e34a3145e0638e2f
akawincent <liwenpu0520@gmail.com>
2023-12-17 19:36:36 +0800

    revert: - use stable safelog

--- utils/mathutils.py
+++ utils/mathutils.py
@@ -1,9 +1,11 @@
 import torch
 
-def safelog(x, eps=1e-12):
-    x[..., 0:1]
-    dim = x.size()
-    eps = torch.Tensor([eps]).expand(dim)
-    x = torch.max(x, torch.Tensor(eps))
-    return torch.log(x)
+# def safelog(x, eps=1e-12):
+#     x[..., 0:1]
+#     dim = x.size()
+#     eps = torch.Tensor([eps]).expand(dim)
+#     x = torch.max(x, torch.Tensor(eps))
+#     return torch.log(x)
 
+def safelog(x, eps=1e-3):
+    return torch.log(x + eps)
